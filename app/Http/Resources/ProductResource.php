7dfd7881f79d500550e862a42ec27ab1a6117d4e
Syed <dcsyedfaraz@gmail.com>
2024-11-25 19:40:00 +0500

    "Refactored ProductResource to group article descriptions and pricing data into arrays"

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -22,9 +22,11 @@ class ProductResource extends JsonResource
                 return $this->image_url;
             }),
             'pzn' => $this->pzn,
-            'article_description_1' => $this->article_description_1,
-            'article_description_2' => $this->article_description_2,
-            'article_description_3' => $this->article_description_3,
+            'descriptions' => [
+                'description_1' => $this->article_description_1,
+                'description_2' => $this->article_description_2,
+                'description_3' => $this->article_description_3,
+            ],
             'price_unit' => $this->price_unit,
             'minimum_order_quantity' => $this->minimum_order_quantity,
             'quantity_unit' => $this->quantity_unit,
@@ -32,12 +34,20 @@ class ProductResource extends JsonResource
             'retail_price' => $this->retail_price,
             'sales_price' => $this->sales_price,
             'tier_code' => $this->tier_code,
-            'quantity_value_1' => $this->quantity_value_1,
-            'net_price_discount_1' => $this->net_price_discount_1,
-            'quantity_value_2' => $this->quantity_value_2,
-            'net_price_discount_2' => $this->net_price_discount_2,
-            'quantity_value_3' => $this->quantity_value_3,
-            'net_price_discount_3' => $this->net_price_discount_3,
+            'pricing' => [
+                [
+                    'quantity_value' => $this->quantity_value_1,
+                    'net_price_discount' => $this->net_price_discount_1,
+                ],
+                [
+                    'quantity_value' => $this->quantity_value_2,
+                    'net_price_discount' => $this->net_price_discount_2,
+                ],
+                [
+                    'quantity_value' => $this->quantity_value_3,
+                    'net_price_discount' => $this->net_price_discount_3,
+                ],
+            ],
             'is_new' => $this->is_new,
             'date_new_entry' => $this->date_new_entry,
             'price_changed' => $this->price_changed,
